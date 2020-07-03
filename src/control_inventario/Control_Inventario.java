d6d415fe113604f69bc3372ab380999fd28f9ceb
Ziliesar <heberzp3@gmail.com>
2020-07-02 20:43:45 -0600

    llamar login

--- src/control_inventario/Control_Inventario.java
+++ src/control_inventario/Control_Inventario.java
@@ -5,6 +5,8 @@
  */
 package control_inventario;
 
+import control_inventario.JFrame.InicioSesion;
+import control_inventario.JFrame.Menu;
 import control_inventario.JFrame.Registrar_Personal;
 
 /**
@@ -17,6 +19,8 @@ public class Control_Inventario {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        control_inventario.JFrame.InicioSesion sesion = new InicioSesion();
+        sesion.setVisible(true);
         
     }
     
