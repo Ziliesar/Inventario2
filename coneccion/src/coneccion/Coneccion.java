7b704d980212cdd04e82aa794d4c8c7ed32d3982
Espacio <heberzp3@gmail.com>
2020-07-05 19:40:16 -0600

    Inicio de sesion con base de datos

---
+++ coneccion/src/coneccion/Coneccion.java
@@ -0,0 +1,22 @@
+package coneccion;
+
+import java.net.Socket;
+
+public class Coneccion {
+    public static void main(String[] args) {
+       String dirWeb = "www.digitalpalace.net";
+        int puerto = 80;
+        
+        try{
+  Socket s = new Socket(dirWeb, puerto);
+  
+  if(s.isConnected()){
+  System.out.println("Conexión establecida con la dirección: " +  dirWeb + " a travéz del puerto: " + puerto);
+}
+  
+  }catch(Exception e){
+  System.err.println("No se pudo establecer conexión con: " + dirWeb + " a travez del puerto: " + puerto);
+}
+    }
+    
+}
