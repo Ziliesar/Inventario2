cf2f5062f0700fda9fccca6435a6350cdab070a3
Espacio <heberzp3@gmail.com>
2020-07-08 19:11:59 -0600

    Calendario y deatalles de factura

--- src/control_inventario/JFrame/calendario.java
+++ src/control_inventario/JFrame/calendario.java
@@ -5,9 +5,20 @@
  */
 package control_inventario.JFrame;
 
+import com.mysql.jdbc.PreparedStatement;
+import com.mysql.jdbc.ResultSetMetaData;
+import static control_inventario.JFrame.Registrar_Ventas.usuarioRE;
+import control_inventario.conexion;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.table.DefaultTableModel;
 
 /**
  *
@@ -20,6 +31,13 @@ public class calendario extends javax.swing.JFrame {
      */
     public calendario() {
         initComponents();
+        
+        
+        
+        
+        
+        
+        
     }
 
     /**
@@ -31,11 +49,21 @@ public class calendario extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jPopupMenu1 = new javax.swing.JPopupMenu();
+        jMenuItem1 = new javax.swing.JMenuItem();
         jdcFecha = new com.toedter.calendar.JDateChooser();
         jLabel1 = new javax.swing.JLabel();
         btnMostrar = new javax.swing.JButton();
-        jScrollPane1 = new javax.swing.JScrollPane();
-        jTable1 = new javax.swing.JTable();
+        jScrollPane2 = new javax.swing.JScrollPane();
+        jtVentas = new javax.swing.JTable();
+
+        jMenuItem1.setText("Ver información");
+        jMenuItem1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMenuItem1ActionPerformed(evt);
+            }
+        });
+        jPopupMenu1.add(jMenuItem1);
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -52,50 +80,63 @@ public class calendario extends javax.swing.JFrame {
             }
         });
 
-        jTable1.setModel(new javax.swing.table.DefaultTableModel(
+        jtVentas.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
-                {null, null, null, null},
-                {null, null, null, null},
-                {null, null, null, null},
-                {null, null, null, null}
+
             },
             new String [] {
-                "Title 1", "Title 2", "Title 3", "Title 4"
+                "N°", "Código", "Vendedor", "Total", "Hora"
             }
-        ));
-        jScrollPane1.setViewportView(jTable1);
+        ) {
+            Class[] types = new Class [] {
+                java.lang.String.class, java.lang.String.class, java.lang.String.class, java.lang.String.class, java.lang.String.class
+            };
+            boolean[] canEdit = new boolean [] {
+                false, false, false, false, false
+            };
+
+            public Class getColumnClass(int columnIndex) {
+                return types [columnIndex];
+            }
+
+            public boolean isCellEditable(int rowIndex, int columnIndex) {
+                return canEdit [columnIndex];
+            }
+        });
+        jtVentas.setComponentPopupMenu(jPopupMenu1);
+        jtVentas.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        jScrollPane2.setViewportView(jtVentas);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addContainerGap(22, Short.MAX_VALUE)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addGap(54, 54, 54)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addGroup(layout.createSequentialGroup()
                         .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 113, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(jdcFecha, javax.swing.GroupLayout.PREFERRED_SIZE, 144, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addContainerGap())
-                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                        .addComponent(btnMostrar)
-                        .addGap(55, 55, 55))
-                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 588, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(20, 20, 20))))
+                        .addComponent(jdcFecha, javax.swing.GroupLayout.PREFERRED_SIZE, 144, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addComponent(jScrollPane2, javax.swing.GroupLayout.DEFAULT_SIZE, 511, Short.MAX_VALUE)
+                            .addComponent(btnMostrar))
+                        .addGap(45, 45, 45)))
+                .addGap(20, 20, 20))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
+                .addGap(20, 20, 20)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addComponent(jdcFecha, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jLabel1))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addComponent(btnMostrar)
-                .addGap(15, 15, 15)
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 258, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(29, Short.MAX_VALUE))
+                .addGap(28, 28, 28)
+                .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 342, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(44, Short.MAX_VALUE))
         );
 
         pack();
@@ -108,8 +149,69 @@ public class calendario extends javax.swing.JFrame {
         DateFormat df = new SimpleDateFormat("yyyy-MM-dd");  
         String text = df.format(date); 
         
+        
+        
+        try {
+            DefaultTableModel modelo=new DefaultTableModel();
+            jtVentas.setModel(modelo);
+            
+            PreparedStatement ps=null;
+            ResultSet rs=null;
+            control_inventario.conexion coon = new conexion(); //where nombre_user='"+user+"' and '"+contra+"'
+            Connection con = coon.conexion();
+            
+            String sql = "SELECT fac.codigo_factura, fac.cog_detalle_fac, CONCAT(per.nombres, \" \", per.apellidos), fac.total, fac.fecha, fac.hora FROM factura fac inner JOIN personal per on fac.identidad=per.identidad WHERE fac.fecha='"+text+"'";
+            ps=(PreparedStatement) con.prepareStatement(sql);
+            rs=ps.executeQuery();
+            
+            ResultSetMetaData rsMd =  (ResultSetMetaData) rs.getMetaData();
+            int cantidadColumnas = rsMd.getColumnCount();
+            modelo.addColumn("Numero");
+            modelo.addColumn("Código");
+            modelo.addColumn("Identidad del vendedor");
+            modelo.addColumn("Total");
+            modelo.addColumn("Hora");
+            
+            while(rs.next()){
+                String[] filas=new String[cantidadColumnas]; 
+                
+                for (int i = 0; i < cantidadColumnas; i++) {
+                    filas[i]=rs.getString(i + 1);
+                }
+                modelo.addRow(filas);
+            }   
+        } catch (SQLException ex) {
+            System.err.println(ex.toString());
+        } 
     }//GEN-LAST:event_btnMostrarActionPerformed
 
+    
+    //CLICK DERECHO
+    private void jMenuItem1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem1ActionPerformed
+        DefaultTableModel tm = (DefaultTableModel) jtVentas.getModel();
+        String dato=String.valueOf(tm.getValueAt(jtVentas.getSelectedRow(),0));
+        
+       
+        
+        control_inventario.conexion cc8 = new conexion(); //where nombre_user='"+user+"' and '"+contra+"'
+        Connection cn8 = cc8.conexion();
+        String identBD8="SELECT cog_detalle_fac FROM factura where codigo_factura='"+dato+"'";  
+        String codigoFac="HHH";
+        try {           
+            Statement st8=cn8.createStatement();
+            ResultSet rs8=st8.executeQuery(identBD8);            
+            while(rs8.next()){
+                codigoFac = rs8.getString(1);  
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(InicioSesion.class.getName()).log(Level.SEVERE, null, ex);
+        } 
+        
+        Registrar_Ventas regisV = new Registrar_Ventas();
+        regisV.setVisible(true);
+    }//GEN-LAST:event_jMenuItem1ActionPerformed
+//FIN POPPUPS
+    
     /**
      * @param args the command line arguments
      */
@@ -148,8 +250,10 @@ public class calendario extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btnMostrar;
     private javax.swing.JLabel jLabel1;
-    private javax.swing.JScrollPane jScrollPane1;
-    private javax.swing.JTable jTable1;
+    private javax.swing.JMenuItem jMenuItem1;
+    private javax.swing.JPopupMenu jPopupMenu1;
+    private javax.swing.JScrollPane jScrollPane2;
     private com.toedter.calendar.JDateChooser jdcFecha;
+    private javax.swing.JTable jtVentas;
     // End of variables declaration//GEN-END:variables
 }
