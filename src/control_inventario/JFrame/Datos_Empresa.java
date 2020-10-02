ec2dff9e83e01eb4ea07cbcfa743b7bac7e4da86
Ziliesar <heberzp3@gmail.com>
2020-10-02 00:57:25 -0600

    datos empresa

--- src/control_inventario/JFrame/Datos_Empresa.java
+++ src/control_inventario/JFrame/Datos_Empresa.java
@@ -92,11 +92,13 @@ public class Datos_Empresa extends javax.swing.JFrame {
         txt_descripcion_emp.setText("");
         txt_imagen_emp.setText("");
         txt_nombre_emp.setText("");
+        txt_id_emp.setText("");
     }
     
     void mostrarDatos(){
         DefaultTableModel modelo=new DefaultTableModel();
        
+        modelo.addColumn("ID Empresa");
         modelo.addColumn("Nombre");
         modelo.addColumn("Descripción");
         modelo.addColumn("Celular");
@@ -105,10 +107,10 @@ public class Datos_Empresa extends javax.swing.JFrame {
         jTable1.setModel(modelo);
         String sql="";
        
-        sql="SELECT nombre, direccion, celular, correo, imagen FROM datos_empresa";
+        sql="SELECT idEmpresa, nombre, direccion, celular, correo, imagen FROM datos_empresa";
      
         
-        String []datos=new String [5];
+        String []datos=new String [6];
         try{
             Statement st=cn.createStatement();
             ResultSet rs=st.executeQuery(sql);
@@ -118,7 +120,8 @@ public class Datos_Empresa extends javax.swing.JFrame {
             datos[2]=rs.getString(3);
             datos[3]=rs.getString(4);
             datos[4]=rs.getString(5);
-            llmarfoto(rs.getString(5));
+            datos[5]=rs.getString(6);
+            llmarfoto(rs.getString(6));
             
             modelo.addRow(datos);
             }
@@ -132,6 +135,48 @@ public class Datos_Empresa extends javax.swing.JFrame {
         Image foto = getToolkit().getImage(img);
         foto = foto.getScaledInstance(110, 110, Image.SCALE_DEFAULT);
         jLabel_foto.setIcon(new ImageIcon(foto));
+    }
+    
+    public void ActualizarDatos(){
+        String id, nombre, direccion, cel, correo, imagen;
+        
+        id = txt_id_emp.getText();
+        nombre = txt_nombre_emp.getText();
+        direccion = txt_descripcion_emp.getText();
+        cel = txt_cel_emp.getText();
+        correo = txt_correo_emp.getText();
+        imagen = txt_imagen_emp.getText();
+        
+        try {
+        
+        String Ssql = "UPDATE datos_empresa SET nombre=?, direccion=?, celular=?, correo=?, imagen=? WHERE idEmpresa=?";
+        
+        PreparedStatement prest = cn.prepareStatement(Ssql);
+        
+        prest.setString(1, nombre);
+        prest.setString(2, direccion);
+        prest.setString(3, cel);
+        prest.setString(4, correo);
+        prest.setString(5, imagen);
+        
+        prest.setString(6, id);
+        
+        
+        if(prest.executeUpdate() > 0){
+        
+            JOptionPane.showMessageDialog(null, "Los datos han sido modificados con éxito", "Operación Exitosa",JOptionPane.INFORMATION_MESSAGE);
+            
+        }else{
+        
+            JOptionPane.showMessageDialog(null, "No se ha podido realizar la actualización de los datos\n"
+            + "Inténtelo nuevamente.", "Error en la operación", JOptionPane.ERROR_MESSAGE);
+        
+        }
+        
+        } catch (SQLException e) {
+            System.out.println("error: "+e);
+        }
+        limpiar();
     }
 
     /**
@@ -164,6 +209,8 @@ public class Datos_Empresa extends javax.swing.JFrame {
         jButton4 = new javax.swing.JButton();
         jScrollPane2 = new javax.swing.JScrollPane();
         jTable1 = new javax.swing.JTable();
+        jLabel7 = new javax.swing.JLabel();
+        txt_id_emp = new javax.swing.JTextField();
 
         jMenuItem1.setText("Seleccionar");
         jMenuItem1.addActionListener(new java.awt.event.ActionListener() {
@@ -216,6 +263,12 @@ public class Datos_Empresa extends javax.swing.JFrame {
         jButton3.setText("regresar");
 
         jButton4.setText("Actualizar");
+        jButton4.setEnabled(false);
+        jButton4.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton4ActionPerformed(evt);
+            }
+        });
 
         jTable1.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
@@ -228,46 +281,23 @@ public class Datos_Empresa extends javax.swing.JFrame {
         jTable1.setComponentPopupMenu(jPopupMenu1);
         jScrollPane2.setViewportView(jTable1);
 
+        jLabel7.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        jLabel7.setText("ID Empresa");
+
+        txt_id_emp.setEnabled(false);
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(37, 37, 37)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jLabel2)
-                    .addComponent(jLabel3))
-                .addGap(23, 23, 23)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(layout.createSequentialGroup()
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addGroup(layout.createSequentialGroup()
-                                .addComponent(txt_nombre_emp, javax.swing.GroupLayout.PREFERRED_SIZE, 200, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                .addGap(18, 18, 18)
-                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                    .addComponent(jLabel4)
-                                    .addComponent(jLabel5)))
-                            .addGroup(layout.createSequentialGroup()
-                                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 200, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                .addGap(18, 18, 18)
-                                .addComponent(jLabel6)))
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 36, Short.MAX_VALUE)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                            .addComponent(txt_cel_emp, javax.swing.GroupLayout.PREFERRED_SIZE, 200, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(txt_correo_emp, javax.swing.GroupLayout.PREFERRED_SIZE, 200, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addGroup(layout.createSequentialGroup()
-                                .addComponent(jLabel_foto, javax.swing.GroupLayout.PREFERRED_SIZE, 111, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                                .addComponent(jButton1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
-                        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(68, 68, 68)
-                        .addComponent(jButton2)
-                        .addGap(18, 18, 18)
-                        .addComponent(jButton3)
-                        .addGap(18, 18, 18)
-                        .addComponent(jButton4)
-                        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))))
+                .addGap(189, 189, 189)
+                .addComponent(jButton2)
+                .addGap(18, 18, 18)
+                .addComponent(jButton3)
+                .addGap(18, 18, 18)
+                .addComponent(jButton4)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                 .addContainerGap(19, Short.MAX_VALUE)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -277,6 +307,35 @@ public class Datos_Empresa extends javax.swing.JFrame {
                     .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                         .addComponent(jLabel1)
                         .addGap(150, 150, 150))))
+            .addGroup(layout.createSequentialGroup()
+                .addGap(32, 32, 32)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jLabel3)
+                    .addComponent(jLabel2)
+                    .addComponent(jLabel7))
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(txt_nombre_emp, javax.swing.GroupLayout.PREFERRED_SIZE, 200, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(txt_id_emp, javax.swing.GroupLayout.PREFERRED_SIZE, 70, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGap(28, 28, 28)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jLabel4)
+                            .addComponent(jLabel5)))
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 200, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(28, 28, 28)
+                        .addComponent(jLabel6)))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(txt_cel_emp, javax.swing.GroupLayout.PREFERRED_SIZE, 200, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(txt_correo_emp, javax.swing.GroupLayout.PREFERRED_SIZE, 200, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jLabel_foto, javax.swing.GroupLayout.PREFERRED_SIZE, 111, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(jButton1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -287,18 +346,25 @@ public class Datos_Empresa extends javax.swing.JFrame {
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                            .addComponent(jLabel2)
-                            .addComponent(txt_nombre_emp, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                            .addComponent(jLabel4)
+                            .addComponent(jLabel7)
+                            .addComponent(txt_id_emp, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                         .addGap(18, 18, 18)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(jLabel3)
-                            .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                    .addGroup(layout.createSequentialGroup()
-                        .addComponent(jLabel4)
-                        .addGap(18, 18, 18)
-                        .addComponent(jLabel5)
-                        .addGap(18, 18, 18)
-                        .addComponent(jLabel6))
+                            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                                .addComponent(jLabel5)
+                                .addComponent(txt_nombre_emp, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                            .addComponent(jLabel2))
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(layout.createSequentialGroup()
+                                .addGap(18, 18, 18)
+                                .addComponent(jLabel6))
+                            .addGroup(layout.createSequentialGroup()
+                                .addGap(15, 15, 15)
+                                .addComponent(jLabel3))
+                            .addGroup(layout.createSequentialGroup()
+                                .addGap(15, 15, 15)
+                                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))))
                     .addGroup(layout.createSequentialGroup()
                         .addComponent(txt_cel_emp, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addGap(18, 18, 18)
@@ -310,14 +376,14 @@ public class Datos_Empresa extends javax.swing.JFrame {
                             .addGroup(layout.createSequentialGroup()
                                 .addGap(18, 18, 18)
                                 .addComponent(jLabel_foto, javax.swing.GroupLayout.PREFERRED_SIZE, 110, javax.swing.GroupLayout.PREFERRED_SIZE)))))
-                .addGap(26, 26, 26)
+                .addGap(32, 32, 32)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jButton2)
                     .addComponent(jButton3)
                     .addComponent(jButton4))
                 .addGap(18, 18, 18)
                 .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 92, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(22, Short.MAX_VALUE))
+                .addContainerGap(16, Short.MAX_VALUE))
         );
 
         pack();
@@ -346,12 +412,26 @@ public class Datos_Empresa extends javax.swing.JFrame {
         // TODO add your handling code here:
         int filaSelec = jTable1.getSelectedRow();
         
-        txt_nombre_emp.setText(jTable1.getValueAt(filaSelec, 0).toString());
-        txt_descripcion_emp.setText(jTable1.getValueAt(filaSelec, 1).toString());
-        txt_cel_emp.setText(jTable1.getValueAt(filaSelec, 1).toString());
-        txt_correo_emp.setText(jTable1.getValueAt(filaSelec, 1).toString());
-        txt_imagen_emp.setText(jTable1.getValueAt(filaSelec, 1).toString());
+        txt_id_emp.setText(jTable1.getValueAt(filaSelec, 0).toString());
+        txt_nombre_emp.setText(jTable1.getValueAt(filaSelec, 1).toString());
+        txt_descripcion_emp.setText(jTable1.getValueAt(filaSelec, 2).toString());
+        txt_cel_emp.setText(jTable1.getValueAt(filaSelec, 3).toString());
+        txt_correo_emp.setText(jTable1.getValueAt(filaSelec, 4).toString());
+        txt_imagen_emp.setText(jTable1.getValueAt(filaSelec, 5).toString());
+        
+        jButton2.setEnabled(false);
+        jButton4.setEnabled(true);
     }//GEN-LAST:event_jMenuItem1ActionPerformed
+
+    private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
+        // TODO add your handling code here:
+        ActualizarDatos();
+        
+        jButton2.setEnabled(false);
+        jButton4.setEnabled(true);
+        
+        mostrarDatos();
+    }//GEN-LAST:event_jButton4ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -399,6 +479,7 @@ public class Datos_Empresa extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel4;
     private javax.swing.JLabel jLabel5;
     private javax.swing.JLabel jLabel6;
+    private javax.swing.JLabel jLabel7;
     private javax.swing.JLabel jLabel_foto;
     private javax.swing.JMenuItem jMenuItem1;
     private javax.swing.JPopupMenu jPopupMenu1;
@@ -408,6 +489,7 @@ public class Datos_Empresa extends javax.swing.JFrame {
     private javax.swing.JTextField txt_cel_emp;
     private javax.swing.JTextField txt_correo_emp;
     private javax.swing.JTextArea txt_descripcion_emp;
+    private javax.swing.JTextField txt_id_emp;
     private javax.swing.JTextField txt_imagen_emp;
     private javax.swing.JTextField txt_nombre_emp;
     // End of variables declaration//GEN-END:variables
